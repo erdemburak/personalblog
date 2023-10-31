645ead361bf950147662c17688082ac117e3ae69
erdemburak <burakerdem6@hotmail.com>
2023-10-31 17:26:29 +0300

    project - personalblog

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import thiefpng from '../images/thief.png'
+
+function Navbar() {
+    return (<>
+        <div>
+            <img src={thiefpng} />
+        </div>
+    </>)
+}
+
+export default Navbar
