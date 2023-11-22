eb71194a6882c37954f433e63b406225dfb6acca
erdemburak <burakerdem6@hotmail.com>
2023-11-22 22:44:15 +0300

    Navbar için css düzenlemeleri yapıldı

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import thiefpng from '../images/thief.png'
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
+
     return (<>
         <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: 'rgba(27, 28, 32, 0.7)', height: '70px' }}>
             <div className='blog-navbar' style={{ display: 'flex', alignItems: 'center', width: '90%' }}>
@@ -14,18 +15,18 @@ function Navbar() {
                     </Link>
 
                 </div>
-                <div>
-                    <ul className='navbar-links'>
-                        <li><Link to="/" className='no-underline'>About</Link></li>
-                        <li><Link to="/skills" className='no-underline'>Skills</Link></li>
-                        <li><Link to="/education" className='no-underline'>Education</Link></li>
-                        <li><Link to="/experience" className='no-underline'>Experience</Link></li>
-                        <li><Link to="/projects" className='no-underline'>Projects</Link></li>
-                        <li><Link to="/contact" className='no-underline'>Contact</Link></li>
+                <div >
+                    <ul className='navbar-links' >
+                        <li><NavLink to="/" className='navbar-button'>About</NavLink></li>
+                        <li><NavLink to="/skills" className='navbar-button'>Skills</NavLink></li>
+                        <li><NavLink to="/education" className='navbar-button'>Education</NavLink></li>
+                        <li><NavLink to="/experience" className='navbar-button'>Experience</NavLink></li>
+                        <li><NavLink to="/projects" className='navbar-button'>Projects</NavLink></li>
+                        <li><NavLink to="/contact" className='navbar-button'>Contact</NavLink></li>
                     </ul>
                 </div>
             </div>
-        </div>
+        </div >
     </>)
 }
 
