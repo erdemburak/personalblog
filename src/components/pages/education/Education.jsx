5a0b6cb6c16a130208e709d73c258b5a731e1530
erdemburak <burakerdem6@hotmail.com>
2023-11-22 22:46:02 +0300

    Eğitimler eklendi

--- src/components/pages/education/Education.jsx
+++ src/components/pages/education/Education.jsx
@@ -2,24 +2,27 @@ import React from 'react'
 import './Education.css';
 
 function Education() {
+
     return (<>
 
-        <div className='education'>
+        <div className='education hovered-div'>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <h1>Duzce Univercity</h1>
                 <h5>Sep 2014 - Jun 2019</h5>
             </div>
+            <h4 style={{ opacity: '0.7' }}>Bachelor's degree</h4>
             <h3>Computer Engineering</h3>
             <p>
-                I have graduated from Duzce Univercity Computer Engineering department in 2019.At university, I learned about subjects such as C++/C# coding, Algorithm logic, Network structures and OOP.
+                I have graduated from Duzce Univercity Computer Engineering department in 2019. I learned about subjects such as OOP, C++/C# coding, Algorithm logic and Network structures.
             </p>
             <p><span>Skills: </span> OOP · C++ · C# · SQL · Python · Algorithm · Network</p>
         </div>
-        <div className='education'>
+        <div className='education hovered-div'>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <h1>Cubuk Anatolian Industrial Vocational High School</h1>
+                <h1>Cubuk Anatolian Industrial Vocational</h1>
                 <h5>Sep 2009 - Jun 2013</h5>
             </div>
+            <h4 style={{ opacity: '0.7' }}>High School</h4>
             <h3>Information Technologies</h3>
             <p>
                 I have graduated from Cubuk Anatolian Industrial Vocational High School Information Technologies department in 2013. I have learned the basics of programming and computer science in this department.
@@ -28,12 +31,12 @@ function Education() {
             <p><span>Skills: </span> HTML · Dreamweaver · Hardware</p>
         </div>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '30px' }}>
-            <div style={{ fontFamily: 'sans-serif', fontSize: '26px' }}>
+            <div className='courses'>
                 Courses
-                <hr style={{ marginTop: '0' }} />
+                <hr style={{ marginTop: '0', borderColor: '#a0632e' }} />
             </div>
         </div>
-        <div className='education'>
+        <div className='education hovered-div'>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <h1>Bilge Adam Academy</h1>
                 <h5>Apr 2022 - Oct 2022</h5>
@@ -44,7 +47,7 @@ function Education() {
             </p>
             <p><span>Skills: </span> Object-Oriented Programming (OOP) · Java · Spring Boot · Spring Security · Spring Rest · HTML · Bootstrap · Jakarta Persistence · Primefaces · Thymeleaf · JPA · Hibernate · PostgreSQL · SQL</p>
         </div>
-        <div className='education'>
+        <div className='education hovered-div'>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <h1>TechCareer</h1>
                 <h5>Sep 2023 - Nov 2023</h5>
